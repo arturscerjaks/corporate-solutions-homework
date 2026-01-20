9ecb280d87edadaeefa228bab4db6eaecffc3521
Arturs Cerjaks <arturscerjaks@gmail.com>
2026-01-20 15:08:37 +0200

    Adjust AuthController to work with API tokens

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -62,5 +65,55 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+
+    /**
+     * Create a personal access token if credentials match, logout previous
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createToken(Request $request): JsonResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        // It would be unsafe to say whether an email or password is registered here
+        if (! $user || ! Hash::check($credentials['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        // Only let 1 token persist per account
+        $user->tokens()->where('name', (string) $user->id)->delete();
+
+        $token = $user->createToken((string) $user->id)->plainTextToken;
+
+        return response()->json([
+            'message' => "You've logged in successfully.",
+            'token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+     * Logout user by deleting their personal access token(s)
+     *
+     * @return JsonResponse
+     */
+    public function deleteToken(Request $request): JsonResponse
+    {
+        $request->user()
+            ->currentAccessToken()
+            ->delete();
+
+        return response()->json([
+            'message' => "You've logged out successfully."
+        ]);
     }
 }
